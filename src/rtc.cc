eb6f59a9ced4e00a6c4b3fef3ac70c6970cd1def
Pasquale Boemio <boemianrapsodi@gmail.com>
2013-07-12 12:07:10 +0200

    headers

--- src/rtc.cc
+++ src/rtc.cc
@@ -1,3 +1,30 @@
+/***
+ *             __         _
+ *      _   _ / _| ___   (_)___
+ *     | | | | |_ / _ \  | / __|
+ *     | |_| |  _| (_) | | \__
+ *      \__,_|_|  \___(_)/ |___/
+ *                     |__/
+ *
+ *     Proudly developed by:
+ *
+ *     boemianrapsodi   @gmail.com
+ *     b3by.in.th3.sky  @gmail.com
+ *
+ *
+ *     GitHub URL:
+ *
+ *     https://github.com/organizations/ufojs
+ *
+ *   This project is completely open source, and can be
+ *   found on GitHub. So, if you like it and would like
+ *   to collaborate, just let us know. We're totally on
+ *   this sh*t, and appreciate comments and suggestions
+ *   as much as we do for bacon.
+ *   Do you know what else do we like? Bacon.
+ *
+ */
+
 #define BUILDING_NODE_EXTENSION
 #include <node.h>
 #include "peerconnection.h"
